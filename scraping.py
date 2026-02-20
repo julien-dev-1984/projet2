47d8c5b3cc0f17538c2d81543b3ceacb8ef02314
Julien <julien.laurensdj@gmail.com>
2026-02-20 17:54:20 +0100

    Récupération des données d'une page produit avec beautifulsoup

--- scraping.py
+++ scraping.py
@@ -4,25 +4,8 @@ from bs4 import BeautifulSoup
 
 
 url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-
-# Simulation du navigateur web utilisé pour éviter d'être bloqué par le site web
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-}
-
-response = requests.get(url, headers=headers)
-
-if response.status_code == 200:
-    print("La page a été récupérée avec succès !")
-    # Affichage du contenu de la page web
-    html_content = response.text
-
-else:
-    print(f"Erreur lors de la récupération de la page : {response.status_code}")
+response = requests.get(url)
 
 
-
-soup = BeautifulSoup(html_content, "html.parser")
-
-titre = soup.find_all("h1")[0].text
-print(f"Le titre du livre est : {titre}")
+soup = BeautifulSoup(response.text, "html.parser")
+print(soup.prettify())
