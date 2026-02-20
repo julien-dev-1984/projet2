b9758e29ff5b3e070badbc554bd6271fb9e78799
Julien <julien.laurensdj@gmail.com>
2026-02-20 15:52:39 +0100

    Correction de l'import

--- scraping.py
+++ scraping.py
@@ -1,5 +1,7 @@
 # Utilisation de la bibliothèque requests pour récupérer l'url de la page web
 import requests
+from bs4 import BeautifulSoup
+
 
 url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 
